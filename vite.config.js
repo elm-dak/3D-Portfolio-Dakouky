c0f92cc4543caa8ab6f28438116e585eb891b6a5
elm-dak <mestafa.wydad66@gmail.com>
2023-12-22 19:26:11 +0100

    m-2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 80,
+    },
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
